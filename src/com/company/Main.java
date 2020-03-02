91360087d279c21f58a1310f72fb61aa1d17128b
Xavier O <xavier.odonovan.2021@student.lhschools.org>
2020-03-02 13:42:45 -0500

    IntelliJ commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("Hello World,");
+
+        System.out.print("My name is ");
+        System.out.println("Xavier O'Donovan,");
+
+        System.out.println("I went to Cedar Point for the second time,");
+
+        System.out.println("I finally got an AC unit for my room,");
+
+        System.out.println("And I played a lot more video games.");
+    }
+}
